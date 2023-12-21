65ae1bf34d920231ac3d4470c4492fc110418e0e
Rajeev Paudel <rajeev11paudel@gmail.com>
2023-12-21 16:35:15 +0545

    Added Modal with login as well as Signup Form, also learned to add some cards inside the page

--- index.php
+++ index.php
@@ -14,9 +14,49 @@
 </head>
 
 <body>
-    <?php  include'partials/header.php';  ?>
-    
-    <?php  include'partials/footer.php';  ?>
+    <?php include 'partials/header.php'; ?>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://source.unsplash.com/2400x700/?programming,python" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://source.unsplash.com/2400x700/?programming,java" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://source.unsplash.com/2400x700/?programming,html" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <div class="container my-3">
+        <h1 class="text-center my-3">iDiscuss - Categories</h1>
+        <div class="row">
+
+            <!-- use a for loop to iterate through the categories -->
+            <div class="col-md-4">
+                <div class="card my-3" style="width: 18rem;">
+                    <img src="https://source.unsplash.com/500x400/?programming,python" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                        <a href="#" class="btn btn-primary">View Threads</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <?php include 'partials/footer.php'; ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,7 @@
 <?php
 echo '
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">iDiscuss</a>
+    <a class="navbar-brand" href="index.php">iDiscuss</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -10,15 +10,15 @@ echo '
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+                <a class="nav-link" href="about.php">About</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
+                    Categories
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item" href="#">Action</a>
@@ -28,7 +28,7 @@ echo '
                 </div>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#" tabindex="-1" >Contact</a>
+                <a class="nav-link" href="contact.php" >Contact</a>
             </li>
         </ul>
         <div class="row mx-2">
@@ -36,12 +36,14 @@ echo '
         <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
         </form>
-        <button class="btn btn-outline-success mx-2">Signup</button>
-        <button class="btn btn-outline-success ml-2">Login</button>
+        <button class="btn btn-outline-success mx-2" data-toggle="modal" data-target="#signupModal">Signup</button>
+        <button class="btn btn-outline-success ml-2" data-toggle="modal" data-target="#loginModal">Login</button>
         </div>
     </div>
 </nav>';
 //- Navbar Ends
+include 'partials/signupModal.php';
+include 'partials/loginModal.php';
 
 
 ?>
